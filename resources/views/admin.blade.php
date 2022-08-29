c4d354b9ae5c9657b87d2bcc6ff308e961a95b57
Maruf <maruf.akib73@gmail.com>
2022-08-29 12:39:28 +0600

    Calender update

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -1,33 +1,32 @@
-{{-- <x-app-layout> --}}
+
 
   @extends('layouts.app')
 
   @section('content')
 
 
-  
-{{-- 
-     <div class="mx-5" style="background-color: azure"> <h1>  <strong> Admin Dashboard</strong></h1>
-    </div>  --}}
-    
 
-
-
+  <h3 class="mb-0 heart" style="color: rgb(95, 197, 197)" ><strong>{{'Calender'}}</strong> </h3>
+         
+         <div class="my-5" style="background-color:rgb(235, 211, 232); border:1px solid black">
+       
+        <div  id="calender"></div>
  
+      </div>
 
-   
-{{-- 
-</div>
 
-</div>
-</div> --}}
+      <style>
+        .heart{
+          font-family: "Audiowide", sans-serif;
+        }
+      </style>
 
 
 @stop
 
 
     
-    {{-- </x-app-layout> --}}
+    
     
     
     
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,11 +1,31 @@
-<x-app-layout>
 
-    <div style="background-color: azure"> <h1>  <strong> User Dashboard</strong></h1>
-    </div>
+  @extends('layouts.app')
+
+  @section('content')
+
+
+
+  <h3 class="mb-0 heart" style="color: rgb(95, 197, 197)" ><strong>{{'Calender'}}</strong> </h3>
+         
+         <div class="my-5" style="background-color:rgb(235, 211, 232); border:1px solid black">
+       
+        <div  id="calender"></div>
+ 
+      </div>
+
+
+      <style>
+        .heart{
+          font-family: "Audiowide", sans-serif;
+        }
+      </style>
+
+
+@stop
+
+
     
-                  
     
-    </x-app-layout>
     
     
     
